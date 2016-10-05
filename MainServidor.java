b6951847faecbcc1a9b80e49f99cab340ad650a4
stevega94 <stevega94@gmail.com>
2016-10-05 04:42:50 -0600

    Add files via upload
    Servidor, versión inicial, falta por añadir lo relativo al JSON, y hacer próximas mejoras.

---
+++ MainServidor.java
@@ -0,0 +1,17 @@
+package sockets.main;
+
+import java.io.IOException;
+import sockets.servidor.Servidor;
+
+
+//Clase principal que hará uso del servidor
+public class MainServidor
+{
+    public static void main(String[] args) throws IOException
+    {        
+        Servidor serv = new Servidor(); //Se crea el servidor
+        
+        System.out.println("Iniciando servidor\n");
+        serv.startServer(); //Se inicia el servidor
+    }
+}
